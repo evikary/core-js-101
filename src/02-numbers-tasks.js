c328ea34d7c3b9eb81eff57646deb98cb043f8b3
evikary <daria_more10@mail.ru>
2023-10-02 16:04:47 +0300

    add numbers tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -19,8 +19,9 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  const res = width * height;
+  return res;
 }
 
 
@@ -35,8 +36,9 @@ function getRectangleArea(/* width, height */) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  const res = 2 * Math.PI * radius;
+  return res;
 }
 
 /**
@@ -52,8 +54,11 @@ function getCircleCircumference(/* radius */) {
  *  -3, 3  => 0
  */
 function getAverage(/* value1, value2 */) {
+  // const res = (value1 + value2) / 2;
+  // return res;
   throw new Error('Not implemented');
 }
+
 
 /**
  * Returns a distance between two points by cartesian coordinates.
@@ -70,8 +75,9 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const res = Math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2));
+  return res;
 }
 
 /**
@@ -86,8 +92,9 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  const res = -b / a;
+  return res;
 }
 
 
@@ -125,10 +132,15 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
-}
+// function getLastDigit(value) {
+//   const res = value.toString(10);
+//   return (+res[res.length - 1]);
+// }
 
+function getLastDigit(value) {
+  const res = value % 10;
+  return res;
+}
 
 /**
  * Returns a number by given string representation.
@@ -141,8 +153,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return +value;
 }
 
 /**
@@ -158,8 +170,9 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  const res = Math.sqrt((a ** 2) + (b ** 2) + (c ** 2));
+  return res;
 }
 
 
@@ -180,8 +193,10 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const m = 10 ** pow;
+  const res = Math.round(Math.floor(num) / m) * m;
+  return res;
 }
 
 /**
@@ -201,9 +216,26 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+// function isPrime(n) {
+//   let res = true;
+//   for (let i = 2; i < n; i += 1) {
+//     if (n % i === 0) {
+//       res = false;
+//       return res;
+//     }
+//   }
+//   return res;
+// }
+
+function isPrime(n) {
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
+
 
 /**
  * Tries to convert value to number and returns it if conversion was successful;
@@ -220,9 +252,17 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (Number.isNaN(parseInt(value, 10))) {
+    return def;
+  }
+  return parseInt(value, 10);
 }
+
+// function toNumber(value, def) {
+//   const res = Number(value);
+//   return Number.isNaN(res) ? def : res;
+// }
 
 module.exports = {
   getRectangleArea,
